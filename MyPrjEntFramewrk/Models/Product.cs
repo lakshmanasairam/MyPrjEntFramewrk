d58a98d5e274dccefb4ce0e9180bcbab339758df
lakshmanasairam <sairamkota234@gmail.com>
2020-09-29 10:30:01 +0530

    Adding Product Model And Edited Index.cshtml-> (Home)

---
+++ MyPrjEntFramewrk/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPrjEntFramewrk.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+
+        
+    }
+}
